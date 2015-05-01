995259374c720d5f1a47432261baa748659c54b4
刘献亭 <liuxianting@1handcar.com>
2015-05-01 11:13:11 +0800

    评论乱码修复，新鲜事评论实现XXOO

--- app/src/main/java/com/socks/jiandan/net/Request4PushFreshComment.java
+++ app/src/main/java/com/socks/jiandan/net/Request4PushFreshComment.java
@@ -60,15 +60,7 @@ public class Request4PushFreshComment extends Request<Boolean> {
 	 */
 	public static String getRequestURL(String post_id, String parent_id,String parent_name,
 									   String name,String email, String content) {
-		//方法1 转义
-		//content= MessageFormat.format("%40%3Ca+href%3D%27%23comment-{0}%27%3E{1}%3C%2Fa%3E%3A+{2}",parent_id,parent_name, content);
-		//方法2 URLEncoder
 		content= MessageFormat.format("@<a href=\"#comment-{0}\">{1}</a>: {2}",parent_id,parent_name, content);
-		try {
-			content = URLEncoder.encode(content, "utf-8");
-		}catch (Exception ex){
-			Logger.d("URLEncoder error");
-		}
 		return  getRequestURLNoParent(post_id,name,email,content);
 	}
 
@@ -78,9 +70,17 @@ public class Request4PushFreshComment extends Request<Boolean> {
 	 * @return
 	 */
 	public static String getRequestURLNoParent(String post_id,String name, String email,String content) {
-		content = MessageFormat.format("{0}&post_id={1}&content={2}&email={3}&name={4}",
+		//方法1 转义
+		//content= MessageFormat.format("%40%3Ca+href%3D%27%23comment-{0}%27%3E{1}%3C%2Fa%3E%3A+{2}",parent_id,parent_name, content);
+		//方法2 URLEncoder（更优）
+		try {
+			name = URLEncoder.encode(name, "utf-8");
+			content=URLEncoder.encode(content, "utf-8");
+		}catch (Exception ex){
+			Logger.d("URLEncoder error");
+		}
+		return   MessageFormat.format("{0}&post_id={1}&content={2}&email={3}&name={4}",
 				Comment4FreshNews.URL_PUSH_COMMENT, post_id, content, email, name);
-		return   content;
 	}
 
 
