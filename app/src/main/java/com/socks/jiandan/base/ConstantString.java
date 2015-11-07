ddcb34258826d7c6b06141b58d5c1629316651fa
ZhaoKaiQiang <419927089@qq.com>
2015-11-07 20:00:22 +0800

    将评论列表合为一个

---
+++ app/src/main/java/com/socks/jiandan/base/ConstantString.java
@@ -0,0 +1,24 @@
+package com.socks.jiandan.base;
+
+public interface ConstantString {
+
+    String COMMENT_FAILED = "神秘力量导致评论失败 ⊙︿⊙";
+    String VOTE_FAILED = "神秘力量导致投票失败 ⊙︿⊙";
+    String LOAD_FAILED = "神秘力量导致加载失败 ⊙︿⊙";
+    String SAVE_FAILED = "神秘力量导致保存失败 ⊙︿⊙";
+    String LOAD_SHARE = "神秘力量导致分享失败 ⊙︿⊙";
+
+    String INPUT_TOO_SHORT = "你的..太短..";
+    String COPY_SUCCESS = "复制成功";
+    String SAVE_SUCCESS = "保存成功";
+    String SAVE_SMALL_SUCCESS = "保存缩略图成功";
+    String SHARE_TAIL = "分享自煎蛋增强版";
+    String VOTE_XX = "疼...轻点插";
+    String VOTE_OO = "顶的好舒服~";
+    String VOTE_REPEAT = "投过票了";
+    String WHAT_HAPPEN = "卧槽，发生了什么！";
+    String LOAD_NO_NETWORK = "无网络，当前为缓存数据";
+
+    String DATA_FRESHNEWS = "FreshNews";
+
+}
